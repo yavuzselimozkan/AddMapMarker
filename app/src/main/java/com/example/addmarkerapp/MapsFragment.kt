480e41c7dc203af7757533ef9d34236549ec3004
Yavuz Selim Ozkan <ozkanyavuzselim99@gmail.com>
2024-10-28 12:18:13 +0300

    Application updated and completed

--- app/src/main/java/com/example/addmarkerapp/MapsFragment.kt
+++ app/src/main/java/com/example/addmarkerapp/MapsFragment.kt
@@ -3,6 +3,7 @@ package com.example.addmarkerapp
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context.LOCATION_SERVICE
+import android.content.Context.MODE_PRIVATE
 import android.content.pm.PackageManager
 import android.location.LocationListener
 import android.location.LocationManager
@@ -16,16 +17,20 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.PopupWindow
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.addmarkerapp.Model.Marker
 import com.example.addmarkerapp.databinding.FragmentMapsBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MapsFragment : Fragment() {
 
@@ -34,31 +39,49 @@ class MapsFragment : Fragment() {
 
     private var markers = ArrayList<Marker>()
     private var locationManager: LocationManager? = null
+    private lateinit var mMap : GoogleMap
 
+    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        loadMarkers()
+        mMap = googleMap
+
+        //lokasyonu mavi olarak gösterecek
+        googleMap.isMyLocationEnabled = true
+
+        //en son konumu gösterecek
+        locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)?.let {
+            val location = LatLng(it.latitude,it.longitude)
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15f))
+        }
+
+        //marker oluşturulacak
+        googleMap.setOnMapClickListener {
+            val marker = Marker()
+            marker.location = it
+            showDetail(marker,true)
+        }
+
+        //markera basıldığında detay gösterecek
+        googleMap.setOnMarkerClickListener {
+            //burada her bir marker ın tag ını index olarak aldık.
+            showDetail(markers[it.tag as Int],false)
+            //true dönerek basma işleminin tamamlandığını gösterir ve tag gözükmemiş olur.
+            true
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        locationManager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager
 
         requestPermission()
     }
 
     private fun requestPermission()
     {
-        locationManager = activity?.getSystemService(LOCATION_SERVICE) as LocationManager
 
         if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
@@ -86,14 +109,23 @@ class MapsFragment : Fragment() {
 
         if(requestCode == 1)
         {
-            if(!grantResults.isEmpty() && grantResults[0],grantResults[1] == Man)
-        }
+            val fineLocationGranted = grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            val coarseLocationGranted = grantResults.size > 1 && grantResults[1] == PackageManager.PERMISSION_GRANTED
 
-        startLocationRequest()
+            if (fineLocationGranted && coarseLocationGranted)
+            {
+                startLocationRequest()
+            }
+            else
+            {
+                Toast.makeText(requireContext(),"İzin Verilmedi.",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     var locationListener = LocationListener{
-        //TODO
+        val location = LatLng(it.latitude,it.longitude)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15f))
     }
 
     override fun onCreateView(
@@ -145,11 +177,30 @@ class MapsFragment : Fragment() {
 
     private fun addMarker(marker: Marker, id: Int)
     {
-
+        mMap.addMarker(MarkerOptions().position(marker.location!!))?.tag = id
     }
 
     private fun updateShared()
     {
+        val editor = requireActivity().getSharedPreferences(requireActivity().packageName, MODE_PRIVATE).edit()
+        val json = Gson().toJson(markers)
+        editor.putString("markers",json)
+        editor.apply()
+    }
 
+    private fun loadMarkers()
+    {
+        val str = requireActivity().getSharedPreferences(requireActivity().packageName, MODE_PRIVATE).getString("markers","")
+
+        if(str!!.isNotEmpty())
+        {
+            val t = object:TypeToken<ArrayList<Marker>>(){}.type
+            markers = Gson().fromJson(str,t)
+
+            for(i in 0 ..<markers.size-1)
+            {
+                addMarker(markers[i],i)
+            }
+        }
     }
 }
